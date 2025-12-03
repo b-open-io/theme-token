335f0d182746442f86ffe2570655b900f5b06a6b
Satchmo <rohenaz@gmail.com>
2025-12-03 15:11:08 -0500

    Fix studio layout flex chain - restore dashboard scroll behavior

--- src/app/studio/layout.tsx
+++ src/app/studio/layout.tsx
@@ -144,7 +144,7 @@ export default function StudioLayout({
 			</header>
 
 			{/* Content */}
-			<div className="flex-1">
+			<div className="flex min-h-0 flex-1 flex-col">
 				{children}
 			</div>
 		</div>
